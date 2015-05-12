9a5ff8a2811a6729b7218bb925858a7932fafdbb
Cameron Smith <smithc11@rpi.edu>
2015-05-11 21:54:07 -0400

    remove trailing whitespace

--- getTimeCCIDrp.sh
+++ getTimeCCIDrp.sh
@@ -2,22 +2,22 @@
 run() {
   srun --mpi=none -n $2 $1 &>> work${2}.log
 }
-mem() { 
+mem() {
   run "./mpiWork -k kernels/mem1.so -r 4" $1
 }
-flop() { 
+flop() {
   run "./mpiWork -k kernels/flop1.so -r 400" $1
 }
-p2p() { 
+p2p() {
   export MV2_ENABLE_AFFINITY=0
   run "./mpiWork -k kernels/p2pcomm1.so -r 30 -t 2" $1
   unset MV2_ENABLE_AFFINITY
 }
-col() { 
+col() {
   run "./mpiWork -k kernels/colcomm1.so -r 10" $1
 }
 getAvg() {
-  awk '/realTime/ {sum+=$2; cnt+=1;} END {print "average " sum/cnt}' work${1}.log 
+  awk '/realTime/ {sum+=$2; cnt+=1;} END {print "average " sum/cnt}' work${1}.log
 }
 
 declare -a fns
@@ -27,10 +27,10 @@ fns[3]="p2p"
 fns[4]="col"
 
 workers=2
-for i in "${!fns[@]}"; do   
+for i in "${!fns[@]}"; do
   cat /dev/null > work${workers}.log
   echo -n "${fns[i]} workers $workers "
-  for j in {0..4}; do 
+  for j in {0..4}; do
     ${fns[$i]} $workers
   done
   getAvg $workers
